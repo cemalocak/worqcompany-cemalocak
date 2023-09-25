6f10bb3e51c266a6b58241c112d06d820fc49c88
Cemal Ocak <cemalocak.06@gmail.com>
2023-09-25 10:01:54 +0300

    Code Separation-2: Database Operations Separated

--- scrap/models.py
+++ scrap/models.py
@@ -12,6 +12,25 @@ class Merchant(models.Model):
 	def __str__(self):
 		return self.name
 	
+	def save_from_json(self, data_json):
+		self.name = data_json['product']['merchant']['name']
+		self.city_name = data_json['product']['merchant']['cityName']
+		self.seller_score = data_json['product']['merchant']['sellerScore']
+		self.save()
+		return self
+	
+	def save_other_merchants_from_json(self, data_json):
+		other_merchants = []
+		for merchant in data_json['product']['otherMerchants']:
+			other_merchant = Merchant()
+			other_merchant.name = merchant['merchant']['name']
+			other_merchant.city_name = merchant['merchant']['cityName']
+			other_merchant.seller_score = merchant['merchant']['sellerScore']
+			other_merchant.save()
+			other_merchants.append(other_merchant)
+		return other_merchants
+			
+	
 	class Meta:
 		verbose_name = 'Satıcı'
 		verbose_name_plural = 'Satıcılar'
@@ -50,6 +69,26 @@ class Trendyol_Product(models.Model):
 		except Exception as e:
 			logging.error(e)
 			return False
+		
+
+	def save_from_json(self, data_json):
+		#Ürün Bilgilerinin Alınması
+		self.name = data_json['product']['name']
+		self.brand = data_json['product']['brand']['name']
+		self.category = data_json['product']['category']['hierarchy']
+		self.sellingPrice = data_json['product']['price']['sellingPrice']['value']
+		self.discountedPrice = data_json['product']['price']['discountedPrice']['value']
+
+		#Satıcı Bilgilerinin Kaydedilmesi
+		self.merchant = Merchant().save_from_json(data_json)
+		self.save()
+
+		#Diğer Satıcı Bilgilerinin Kaydedilmesi
+		other_merchants = Merchant().save_other_merchants_from_json(data_json)
+		self.other_merchants.set(other_merchants)
+		self.save()
+		
+		return self
 
 	
 	class Meta:
--- scrap/views.py
+++ scrap/views.py
@@ -12,66 +12,7 @@ def find_product(request):
 		url = request.POST['url']
 		product = Trendyol_Product(url = url)
 		data_json = product.get_product_data()
-
-		product_info = {
-			"name": "",
-			"brand": "",
-			"sellingPrice": "",
-			"discountedPrice": "",
-			"category": "",
-			"merchant": {
-				"name": "",
-				"cityName": "",
-				"sellerScore": ""
-			},
-			"otherMerchants": [],
-			"url": ""
-		}
-
-		#Ürün Bilgilerinin Alınması
-
-		product_name = data_json['product']['name']
-		product_brand = data_json['product']['brand']['name']
-		product_category = data_json['product']['category']['hierarchy']
-		product_sellingPrice = data_json['product']['price']['sellingPrice']['value']
-		product_discountedPrice = data_json['product']['price']['discountedPrice']['value']
-
-		product_info['name'] = product_name
-		product_info['brand'] = product_brand
-		product_info['category'] = product_category
-		product_info['sellingPrice'] = product_sellingPrice
-		product_info['discountedPrice'] = product_discountedPrice
-		product_info['url'] = url
-
-		#Satıcı Bilgilerinin Kaydedilmesi
-		merchant_name = data_json['product']['merchant']['name']
-		merchant_city = data_json['product']['merchant']['cityName']
-		merchant_seller_score = data_json['product']['merchant']['sellerScore']
-
-		product_info['merchant']['name'] = merchant_name
-		product_info['merchant']['cityName'] = merchant_city
-		product_info['merchant']['sellerScore'] = merchant_seller_score
-
-		#Diğer Satıcı Bilgilerinin Kaydedilmesi
-		other_merchants = []
-		for merchant in data_json['product']['otherMerchants']:
-			other_merchant = {
-				"name": "",
-				"cityName": "",
-				"sellerScore": ""
-			}
-
-			name = merchant['merchant']['name']
-			city = merchant['merchant']['cityName']
-			seller_score = merchant['merchant']['sellerScore']
-
-			other_merchant['name'] = name
-			other_merchant['cityName'] = city
-			other_merchant['sellerScore'] = seller_score
-			product_info['otherMerchants'].append(other_merchant)
-
-		print(product_info)
-
+		product.save_from_json(data_json)
 		return redirect('index')
 
 	
